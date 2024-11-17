63f64620e3f8093fcb314b9a174d8bfd6b1a88b8
Kirill Briukhanov <k.bryuhanov@powerinsight.io>
2024-11-17 16:32:53 +0100

    add cross resource refrences

--- config/vsphere_virtual_disk/config.go
+++ config/vsphere_virtual_disk/config.go
@@ -4,9 +4,19 @@ import "github.com/upbound/upjet/pkg/config"
 
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
+
 	p.AddResourceConfigurator("vsphere_virtual_disk", func(r *config.Resource) {
 		r.ShortGroup = "virtualmachine"
 		r.Kind = "VSphereVirtualDisk"
 		r.Version = "v1alpha1"
+		r.References["datacenter"] = config.Reference{
+			Type:      "github.com/kirillinda/provider-vsphere/inventory/v1alpha1.VSphereDatacenter",
+			Extractor: `github.com/upbound/upjet/pkg/resource.ExtractParamPath("name", true)`,
+		}
+		r.References["datastore"] = config.Reference{
+			Type:      "github.com/kirillinda/provider-vsphere/inventory/v1alpha1.VSphereVmfsDatastore",
+			Extractor: `github.com/upbound/upjet/pkg/resource.ExtractParamPath("id", true)`,
+		}
 	})
+
 }
--- config/vsphere_virtual_machine/config.go
+++ config/vsphere_virtual_machine/config.go
@@ -8,6 +8,14 @@ func Configure(p *config.Provider) {
 		r.ShortGroup = "virtualmachine"
 		r.Kind = "VSphereVirtualMachine"
 		r.Version = "v1alpha1"
+		r.References["datacenter"] = config.Reference{
+			Type:      "github.com/kirillinda/provider-vsphere/hostandclustermanagement/v1alpha1.VSphereComputeCluster",
+			Extractor: `github.com/upbound/upjet/pkg/resource.ExtractParamPath("name", true)`,
+		}
+		r.References["datastore"] = config.Reference{
+			Type:      "github.com/kirillinda/provider-vsphere/storage/v1alpha1.VSphereVmfsDatastore",
+			Extractor: `github.com/upbound/upjet/pkg/resource.ExtractParamPath("id", true)`,
+		}
 	})
 
 	p.AddResourceConfigurator("vsphere_virtual_machine_snapshot", func(r *config.Resource) {
