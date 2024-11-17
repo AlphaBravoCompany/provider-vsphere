492518e21f57667399768db1843833be1d7cf5c4
Kirill Briukhanov <k.bryuhanov@powerinsight.io>
2024-11-17 16:41:05 +0100

    fox broken path

--- config/vsphere_virtual_machine/config.go
+++ config/vsphere_virtual_machine/config.go
@@ -9,11 +9,11 @@ func Configure(p *config.Provider) {
 		r.Kind = "VSphereVirtualMachine"
 		r.Version = "v1alpha1"
 		r.References["datacenter"] = config.Reference{
-			Type:      "github.com/kirillinda/provider-vsphere/hostandclustermanagement/v1alpha1.VSphereComputeCluster",
+			Type:      "github.com/kirillinda/provider-vsphere/apis/hostandclustermanagement/v1alpha1.VSphereComputeCluster",
 			Extractor: `github.com/upbound/upjet/pkg/resource.ExtractParamPath("name", true)`,
 		}
 		r.References["datastore"] = config.Reference{
-			Type:      "github.com/kirillinda/provider-vsphere/storage/v1alpha1.VSphereVmfsDatastore",
+			Type:      "github.com/kirillinda/provider-vsphere/apis/storage/v1alpha1.VSphereVmfsDatastore",
 			Extractor: `github.com/upbound/upjet/pkg/resource.ExtractParamPath("id", true)`,
 		}
 	})
